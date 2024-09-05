973213434bcdd6b22f189aa75c334fcc9386bcd0
Abouzar <abouzarmoradian1@gmail.com>
2024-09-04 22:08:55 -0400

    Initial Commit

---
+++ UIKitWithoutStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitWithoutStoryboard
+//
+//  Created by Abouzar Moradian on 9/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
